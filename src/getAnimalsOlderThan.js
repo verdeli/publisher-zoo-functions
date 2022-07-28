ee1dd435f42a22b16d4c78ee66f0179177efc0b9
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2022-07-28 19:39:26 -0300

    Requisito 1 OK

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
-
+console.log(data);
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animals = data.find((element) => (element.name === animal));
+  console.log(animals);
+  const idade = data.residents;
+  console.log(idade);
+  return idade;
 }
+console.log(getAnimalsOlderThan());
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui Verdeli!
+  if (ids.length === 0) {
+    return [];
+  }
+  // console.log(ids);
+  // console.log(species);
+  return ids.map((id) => species.find((animal) => id === animal.id));
 }
-
+// console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274','0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
