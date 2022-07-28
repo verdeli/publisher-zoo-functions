69a0d1e4216ea3ed8822a4a62c94aeb2b84deedc
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2022-07-28 20:23:42 -0300

    Requisito 2 OK

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,14 +1,17 @@
 const data = require('../data/zoo_data');
 
-console.log(data);
+const animais = data.species;
+// console.log(data);
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const animals = data.find((element) => (element.name === animal));
-  console.log(animals);
-  const idade = data.residents;
-  console.log(idade);
-  return idade;
+  const animals = animais.find((element) => (element.name === animal));
+  // console.log(animals);
+  const idade = animals.residents;
+  // console.log(idade);
+  const verificaIdade = idade.every((idades) => idades.age >= age);
+  // console.log(age);
+  return verificaIdade;
 }
-console.log(getAnimalsOlderThan());
+console.log(getAnimalsOlderThan('tigers', 100));
 
 module.exports = getAnimalsOlderThan;
