4c24ba15faa51a053d68b04cca89b43cbfbf3703
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2022-07-30 17:54:50 -0300

    Requisito 10 OK

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const { species } = data;
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const animalFinder = employees.find((animal) => animal.id === id).responsibleFor[0];
+  // return animalFinder;
+  const verify = species.find((bicho) => bicho.id === animalFinder);
+  const { residents } = verify;
+  const residentes = residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(residentes);
 }
 
 module.exports = getOldestFromFirstSpecies;
