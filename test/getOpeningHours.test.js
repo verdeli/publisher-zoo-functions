0a74c6dcdbee5d605c57becedd20bd3c90dfdd64
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2022-07-30 18:49:23 -0300

    Requisito 12,13,16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,6 +1,19 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
+  it('Função Vazia;', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(actual).toEqual(expected);
+  });
   it('Zoo é fechado toda;', () => {
     const actual = getOpeningHours('Monday', '09:00-AM');
     const expected = 'The zoo is closed';
@@ -16,14 +29,34 @@ describe('Testes da função getOpeningHours', () => {
     const expected = 'The zoo is closed';
     expect(actual).toEqual(expected);
   });
-  it('A função não faz diferenciação entre maiúsculas e minúsculas;', () => {
-    const regerx = new RegExp('[^A-Za-z0-9]', '');
-    const actual = 'Maiuscula e Minuscula';
-    expect(actual).toMatch(regerx);
+  it('Testar Quinta-Feira;', () => {
+    const actual = () => getOpeningHours('Thu', '09:00-AM');
+    const expected = ('The day must be valid. Example: Monday');
+    expect(actual).toThrow(expected);
   });
-  // it('As horas serão validadasna nomenclatura Am e PM;', () => {
-  //   const regerx = getOpeningHours('Monday', new RegExp('[^A-Za-z0-9]', ''));
-  //   const actual = 'Monday 9:01 AM';
-  //   expect(actual).toMatch(regerx);
-  // });
+  it('Testar a nossa Friday;', () => {
+    const actual = () => getOpeningHours('Friday', '09:00-ZM');
+    const expected = ('The abbreviation must be \'AM\' or \'PM\'');
+    expect(actual).toThrow(expected);
+  });
+  it('Testar Sabadão;', () => {
+    const actual = () => getOpeningHours('Saturday', 'C9:00-A');
+    const expected = ('The hour should represent a number');
+    expect(actual).toThrow(expected);
+  });
+  it('Testar Dominguera;', () => {
+    const actual = () => getOpeningHours('Sunday', '09:c0-AM');
+    const expected = ('The minutes should represent a number');
+    expect(actual).toThrow(expected);
+  });
+  it('Testar Segunda Braba;', () => {
+    const actual = () => getOpeningHours('Monday', '13:00-AM');
+    const expected = ('The hour must be between 0 and 12');
+    expect(actual).toThrow(expected);
+  });
+  it('Testar terça Linda;', () => {
+    const actual = () => getOpeningHours('Tuesday', '09:60-AM');
+    const expected = ('The minutes must be between 0 and 59');
+    expect(actual).toThrow(expected);
+  });
 });
